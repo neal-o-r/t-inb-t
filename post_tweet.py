bd03b9ad2bbe815a3a8f97203cc8af94a9853cfe
nealo <nealo@spotify.com>
2019-09-13 11:53:31 +0100

    clean up

--- post_tweet.py
+++ post_tweet.py
@@ -1,62 +1,50 @@
 import tweepy as tw
 
+
 def where_are_we():
+    with open("position.txt", "r") as f:
+        counter = int(f.read())
 
-	with open("position.txt", 'r') as f:
+    return counter
 
-		counter = int(f.read())
-
-	return counter
 
 def reset_counter():
+    with open("position.txt", "w") as f:
+        f.write("0")
 
-	with open("position.txt", 'w') as f:
-
-		f.write('0')
 
 def update_position(counter, len_of_tweets):
+    with open("position.txt", "w") as f:
+        f.write(str(int(counter % len_of_tweets)))
 
-	with open("position.txt", 'w') as f:
-
-		f.write(str(int(counter % len_of_tweets)))
 
 def get_tweet(counter):
+    with open("tweets.txt", "r") as f:
+        tweets = f.readlines()
 
-	with open("tweets.txt", 'r') as f:
-
-		tweets = f.readlines()
-
-		
-	return tweets[counter].rstrip('\n'), len(tweets)
+    return tweets[counter].rstrip("\n"), len(tweets)
 
 
 def now_tweet(tweet):
+    with open("api.keys", "r") as f:
+        keys = f.readlines()
 
-	with open("api.keys", 'r') as f:
+    for i, key in enumerate(keys):
+        keys[i] = key.rstrip("\n")
 
-		keys = f.readlines()
-	
+    auth = tw.OAuthHandler(keys[0], keys[1])
+    auth.set_access_token(keys[2], keys[3])
 
-	for i, key in enumerate(keys):
-		keys[i] = key.rstrip('\n')
-	
+    api = tw.API(auth)
 
-	auth = tw.OAuthHandler(keys[0], keys[1])
-	auth.set_access_token(keys[2], keys[3])
-
-	api = tw.API(auth)
+    api.update_status(tweet)
 
 
-	api.update_status(tweet)
+if __name__ == "__main__":
 
+    counter = where_are_we()
 
+    tweet, n = get_tweet(counter)
+    now_tweet(tweet)
 
-counter = where_are_we()
-
-tweet, n = get_tweet(counter)
-
-now_tweet(tweet)
-
-update_position(counter+1, n)
-
-
+    update_position(counter + 1, n)
--- tweetify_tain.py
+++ tweetify_tain.py
@@ -1,21 +1,16 @@
 import re
 from nltk import tokenize
 
-f = open('tainb.txt', 'r')
+f = open("tainb.txt", "r")
 
 text = f.read()
 
 f.close()
 
-text_wo_lb = re.sub('(?!.)\n(?=.)', ' ', text)
+text_wo_lb = re.sub("(?!.)\n(?=.)", " ", text)
 
-#f = open('tain_out.txt','w')
 
-#f.write(text_wo_lb)
-
-#f.close()
-
-f = open('tain_out.txt', 'r')
+f = open("tain_out.txt", "r")
 
 text_arr = f.readlines()
 
@@ -24,71 +19,67 @@ f.close()
 
 def subdivide(tw):
 
-	if len(tw) < 140:
-		return tw
-	
-	short_tw = tw[:140]
-	
-	if ';' in short_tw:
-		ind = short_tw.rfind(';')
-	
-	elif ',' in short_tw:
-                ind = short_tw.rfind(',')
-	
-	else:
-		ind = short_tw.rfind(' ')
-	
-	return [short_tw[:ind]] + [subdivide(tw[ind+1:])]
-			
+    if len(tw) < 140:
+        return tw
+
+    short_tw = tw[:140]
+
+    if ";" in short_tw:
+        ind = short_tw.rfind(";")
+
+    elif "," in short_tw:
+        ind = short_tw.rfind(",")
+
+    else:
+        ind = short_tw.rfind(" ")
+
+    return [short_tw[:ind]] + [subdivide(tw[ind + 1 :])]
+
 
 def flatten_out(thing):
 
+    if thing == []:
+        return thing
+    if isinstance(thing[0], list):
+        return flatten_out(thing[0]) + flatten_out(thing[1:])
 
-	if thing == []:
-		return thing
-	if isinstance(thing[0], list):
-		return flatten_out(thing[0]) + flatten_out(thing[1:])
-    	
-	return thing[:1] + flatten_out(thing[1:])
+    return thing[:1] + flatten_out(thing[1:])
 
 
 def sub_tweets(line):
 
-	subs = tokenize.sent_tokenize(line)	 
+    subs = tokenize.sent_tokenize(line)
 
-	out_sub = []
-	for poss_tw in subs:
+    out_sub = []
+    for poss_tw in subs:
 
-		if len(poss_tw) < 140:
-			out_sub.append(poss_tw)
-			
-		else:
-			out_sub.extend(flatten_out(subdivide(poss_tw)))
+        if len(poss_tw) < 140:
+            out_sub.append(poss_tw)
 
+        else:
+            out_sub.extend(flatten_out(subdivide(poss_tw)))
 
-	return out_sub
-			
+    return out_sub
+
 
 tweet_arr = []
 for line in text_arr:
-	
-	line = re.sub('\n', '', line)
-	line = re.sub(' +', ' ', line)
-	line = line.rstrip(' ').lstrip(' ')
-	
-	if len(line) > 0 and len(line) < 140:
-		tweet_arr.append(line) 
-		continue
-	
-	subs = sub_tweets(line)
-	tweet_arr.extend(subs)
+
+    line = re.sub("\n", "", line)
+    line = re.sub(" +", " ", line)
+    line = line.rstrip(" ").lstrip(" ")
+
+    if len(line) > 0 and len(line) < 140:
+        tweet_arr.append(line)
+        continue
+
+    subs = sub_tweets(line)
+    tweet_arr.extend(subs)
 
 
-f = open('tweets.txt', 'w')
+f = open("tweets.txt", "w")
 
 for i in tweet_arr:
-	f.write(i.rstrip(' ').lstrip(' ') + '\n')
+    f.write(i.rstrip(" ").lstrip(" ") + "\n")
 
-f.close()	
-
-
+f.close()
